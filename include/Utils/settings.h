a5267fbcd01eb1d4b2dd204c8a95c4061a8fff42
Diego Antognini <diegoantognini@gmail.com>
2014-04-21 11:31:40 +0200

    Add tags to settings

--- include/Utils/settings.h
+++ include/Utils/settings.h
@@ -1,16 +1,20 @@
 #ifndef SETTINGS_H
 #define SETTINGS_H
 
+#include <string>
+#include <vector>
+
 class SettingsNeuralNetwork
 {
 public:
-    static const int nbInputs;
     static const int nbOutputs;
     static const int nbHiddenLayers;
     static const int nbNeuronsPerHiddenLayer;
 
     static const double learningRate;
     static const double momentum;
+
+    static const std::vector<std::string> tags;
 };
 
 #endif // SETTINGS_H
